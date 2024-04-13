a07ad202404c4d31898f0dbabb0c777e60faf499
Adam Williamson <awilliam@redhat.com>
2024-04-12 18:59:41 -0700

    Add FEDORA-2024-b156c57ef1 as F40 workaround to fix upgrades
    See https://bugzilla.redhat.com/show_bug.cgi?id=2274833. Upgrades
    from F39 to F40 fail without this.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- lib/utils.pm
+++ lib/utils.pm
@@ -476,7 +476,7 @@ sub get_workarounds {
     my %workarounds = (
         "38" => [],
         "39" => [],
-        "40" => [],
+        "40" => ["FEDORA-2024-b156c57ef1"],
         "41" => [],
     );
     my $advortasks = $workarounds{$version};
