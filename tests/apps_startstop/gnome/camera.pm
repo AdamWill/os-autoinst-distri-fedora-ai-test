dcad60de3051289eb02e1eadf7359ce300b1f2bf
Adam Williamson <awilliam@redhat.com>
2024-11-28 13:39:02 -0800

    F39 EOL cleanup: remove Cheese support
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/apps_startstop/gnome/camera.pm
+++ tests/apps_startstop/gnome/camera.pm
@@ -12,12 +12,10 @@ sub run {
 
     # Start the application
     start_with_launcher('apps_menu_camera');
-    # Check that is started or we see the camera access dialog
-    assert_screen ['apps_run_camera', 'grant_access'];
-    if (match_has_tag 'grant_access') {
-        click_lastmatch;
-        assert_screen 'apps_run_camera';
-    }
+    # Click the camera access dialog
+    assert_and_click 'grant_access';
+    # Check we see the app run
+    assert_screen 'apps_run_camera';
     # Register application
     register_application("gnome-snapshot");
     # Close the application
