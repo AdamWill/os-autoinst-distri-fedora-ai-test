077d3182d6bec7cc70baf4594e978dce804b74f3
Adam Williamson <awilliam@redhat.com>
2025-01-16 12:57:24 -0800

    cockpit: use down instead of up to find the Updates page
    Seems like this might work better? Up is still failing a lot.
    Tested on lab and it looked good.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- lib/cockpit.pm
+++ lib/cockpit.pm
@@ -51,7 +51,7 @@ sub select_cockpit_update {
     assert_screen ["cockpit_software_updates", "cockpit_search"], 120;
     click_lastmatch;
     if (match_has_tag "cockpit_search") {
-        send_key_until_needlematch("cockpit_software_updates", "up", 3, 3);
+        send_key_until_needlematch("cockpit_software_updates", "down", 10, 2);
         click_lastmatch;
     }
     # wait for the updates to download
