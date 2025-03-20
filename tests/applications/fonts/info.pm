f474c9a9ed417feadd4eeb8ea3586c7847430aa6
Adam Williamson <awilliam@redhat.com>
2025-03-19 17:58:30 -0700

    Adjust fonts/info.pm to look for Adwaita, not Cantarell
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/applications/fonts/info.pm
+++ tests/applications/fonts/info.pm
@@ -3,23 +3,23 @@ use strict;
 use testapi;
 use utils;
 
-# This script will show info for the Cantarell font
+# This script will show info for the Adwaita Sans font
 # that should be the among those installed by default.
 
 sub run {
     my $self = shift;
-    # Send the TAB key until the Cantarell font is found.
-    send_key_until_needlematch("fonts_cantarell_font", "tab", 30, 1);
+    # Send the TAB key until the Adwaita Sans font is found.
+    send_key_until_needlematch("fonts_adwaita_font", "tab", 30, 1);
     # Click on the icon
     click_lastmatch();
     # Check that the correct font is shown.
-    assert_screen("fonts_cantarell_shown");
+    assert_screen("fonts_adwaita_shown");
     # Check that various sizes are shown.
-    assert_screen("fonts_cantarell_sizes");
+    assert_screen("fonts_adwaita_sizes");
     # Click on Info to get more information.
     assert_and_click("gnome_button_info");
     # Check for various information on that page.
-    assert_screen("fonts_cantarell_info");
+    assert_screen("fonts_adwaita_info");
 }
 
 sub test_flags {
