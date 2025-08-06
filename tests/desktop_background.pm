e4bb6f5ecf926bc233ae8b4981e0bb3ab6114ec8
Lukas Ruzicka <lruzicka@redhat.com>
2025-08-06 12:12:01 +0200

    Inform about not testing for F43

--- tests/desktop_background.pm
+++ tests/desktop_background.pm
@@ -12,9 +12,12 @@ sub run {
     my $version = get_var('VERSION');
     my $rawrel = get_var('RAWREL');
     return unless ($version ne "Rawhide" && $version ne $rawrel);
-    # temporary for f43 branching
+    # temporary for f43 branching to pass during the branch phase
     # FIXME: Remove when branching has completed
-    return if ($version eq "43");
+    if ($version eq "43") {
+        record_info("Short circuit", "The background is not tested for Fedora 43. We shall switch it back on when we have fully branched.");
+        return;
+    }
     # KDE shows a different version of the welcome center on major upgrades,
     # which breaks this test
     click_lastmatch if (get_var("DESKTOP") eq "kde" && get_var("ADVISORY_OR_TASK") && check_screen "kde_ok", 5);
