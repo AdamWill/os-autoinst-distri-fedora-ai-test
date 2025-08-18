a396c2f41df4a9116141585513988f01868e4a5c
Lukáš Růžička <lruzicka@redhat.com>
2025-08-18 11:20:43 +0200

    Add start-stop test for Kleopatra
    This PR adds a start-stop test for Kleopatra that will replace
    the KGpg application. It does not, however, remove the KGpg
    start-stop test from the suite, as the KGpg is not gone yet.
    We will remove it in a separate commit when time has come.
    
    Fixes: https://pagure.io/fedora-qa/os-autoinst-distri-fedora/issue/405
    
    Change the start command to kleopatra
    
    Move needle to generic space
    
    Remove checking the selection box

---
+++ tests/apps_startstop/kde/kleopatra.pm
@@ -0,0 +1,36 @@
+use base "installedtest";
+use strict;
+use testapi;
+use utils;
+
+# This test checks that Kleopatra starts.
+
+sub run {
+    my $self = shift;
+
+    # Start the application
+    menu_launch_type 'kleopatra';
+    # Check that the Self Test page has appeared.
+    assert_screen("kleopatra_selfcheck_page");
+    # There should be no failed tests, but since this is not the application
+    # test, we will only softfail if they are failed.
+    unless (check_screen("kleopatra_selfcheck_passed", timeout => 30)) {
+        record_soft_failure("Kleopatra selfcheck tests do not pass!");
+    }
+    assert_and_click("kde_continue");
+    wait_still_screen 2;
+
+    # Check that it is started
+    assert_screen 'apps_run_kleopatra';
+    # Close the application
+    quit_with_shortcut();
+}
+
+sub test_flags {
+    return {};
+}
+
+
+1;
+
+# vim: set sw=4 et:
