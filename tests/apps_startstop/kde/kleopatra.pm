3a2f45ef505806349fc81cfde1aaf360bcacaadf
Adam Williamson <awilliam@redhat.com>
2025-09-06 15:33:35 -0700

    kleopatra: handle launch with no self-check page
    In Rawhide we're not seeing the self-check page, we're going
    straight to the app. Handle that.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/apps_startstop/kde/kleopatra.pm
+++ tests/apps_startstop/kde/kleopatra.pm
@@ -11,14 +11,15 @@ sub run {
     # Start the application
     menu_launch_type 'kleopatra';
     # Check that the Self Test page has appeared.
-    assert_screen("kleopatra_selfcheck_page");
-    # There should be no failed tests, but since this is not the application
-    # test, we will only softfail if they are failed.
-    unless (check_screen("kleopatra_selfcheck_passed", timeout => 30)) {
-        record_soft_failure("Kleopatra selfcheck tests do not pass!");
+    assert_screen(["kleopatra_selfcheck_page", "apps_run_kleopatra"]);
+    if (match_has_tag("kleopatra_selfcheck_page")) {
+        # There should be no failed tests, but since this is not the application
+        # test, we will only softfail if they are failed.
+        unless (check_screen("kleopatra_selfcheck_passed", timeout => 30)) {
+            record_soft_failure("Kleopatra selfcheck tests do not pass!");
+        }
+        assert_and_click("kde_continue");
     }
-    assert_and_click("kde_continue");
-    wait_still_screen 2;
 
     # Check that it is started
     assert_screen 'apps_run_kleopatra';
