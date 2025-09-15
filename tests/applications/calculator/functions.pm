ae9e7573848583bb33a8bcd55100985c6a2ea32f
Lukáš Růžička <lruzicka@redhat.com>
2025-09-15 14:07:37 +0200

    Change subroutine to test Functions
    Calculator changed the way functions are organized within the
    application which broke the current Calculator test.
    
    This fixes the subroutine to test for Functions and makes the
    test reliable again.
    
    Fixes: https://pagure.io/fedora-qa/os-autoinst-distri-fedora/issue/428

--- tests/applications/calculator/functions.pm
+++ tests/applications/calculator/functions.pm
@@ -7,14 +7,15 @@ use utils;
 # can use pre-selected functions.
 
 sub use_function {
-    my $function = shift;
+    my ($function, $where, $expression) = @_;
     assert_and_click("calc_button_fxunified");
-    send_key_until_needlematch("calc_function_$function", "down", 50);
-    click_lastmatch();
-    assert_and_click("calc_button_five");
+    assert_and_click("calc_functions_$where");
+    assert_and_click("calc_select_function_$function");
+    type_safely($expression);
     send_key("ret");
     assert_screen("calc_result_$function");
     assert_and_click("calc_button_clear");
+    sleep(1);
 }
 
 sub run {
@@ -22,9 +23,12 @@ sub run {
     # Wait until everything settles.
     sleep 5;
 
-    use_function("sqrt");
-    use_function("arg");
-    use_function("cos");
+    use_function("cos", "trigonometry", "45");
+    use_function("re", "complex", "20");
+    use_function("ones", "programming", "5");
+    use_function("round", "rounding", "5.55");
+    use_function("median", "statistics", "1;2;4;5");
+    use_function("sqrt", "sundry", "5");
 }
 
 sub test_flags {
