4b3a595626d08e7149062a795daf08b2f6171498
Adam Williamson <awilliam@redhat.com>
2025-09-26 15:12:09 -0700

    blivet software RAID: create ESP and /boot as RAID (#417)
    As requested by @kparal in
    https://pagure.io/fedora-qa/os-autoinst-distri-fedora/issue/417 ,
    this matches the modified test case by creating the EFI system
    partition and /boot partition on RAID devices, as well as the
    root partition. Also let's make them a bit bigger; if we leave
    the value at 512 they actually get created 511MB big (dunno why)
    and anaconda complains. Anyway, bigger sizes are prudent these
    days.
    
    We also need to create a BIOS boot partition on the second disk
    in the BIOS test now. Apparently if /boot is RAID-ed, there has
    to be a BIOS boot partition on each disk. This is a bit of an
    ugly implementation, oh well.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/disk_custom_blivet_software_raid.pm
+++ tests/disk_custom_blivet_software_raid.pm
@@ -12,17 +12,27 @@ sub run {
 
     if (get_var("UEFI")) {
         # if we're running on UEFI, we need esp
-        custom_blivet_add_partition(size => 512, mountpoint => '/boot/efi', filesystem => 'efi_filesystem');
+        custom_blivet_add_partition(raid1 => 1, size => 1024, mountpoint => '/boot/efi', filesystem => 'efi_filesystem');
     }
     elsif (get_var("OFW")) {
         custom_blivet_add_partition(size => 4, filesystem => 'ppc_prep_boot');
     }
     else {
         # from anaconda-37.12.1 onwards, GPT is default for BIOS
-        # installs, so we need a biosboot partition
+        # installs, so we need biosboot partitions on each disk
         custom_blivet_add_partition(size => 1, filesystem => 'biosboot');
+        wait_still_screen 3;
+        assert_and_click "anaconda_blivet_disk_2";
+        wait_still_screen 3;
+        custom_blivet_add_partition(size => 1, filesystem => 'biosboot');
+        # go back to disk 1 as custom_blivet_add_partition kinda expects
+        # us to be there
+        wait_still_screen 3;
+        assert_and_click "anaconda_blivet_disk_1";
+        wait_still_screen 3;
+        assert_and_click "anaconda_blivet_free_space";
     }
-    custom_blivet_add_partition(size => 512, mountpoint => '/boot');
+    custom_blivet_add_partition(raid1 => 1, size => 1024, mountpoint => '/boot');
     custom_blivet_add_partition(raid1 => 1, mountpoint => '/');
 
     assert_and_click "anaconda_spoke_done";
