25d1998b870dc96f4687b9601e11a72dc5b5b6cb
Adam Williamson <awilliam@redhat.com>
2025-12-01 11:54:47 -0800

    Handle known failure of vkcube --validate on Fedora 42
    The new vkcube --validate test fails on Fedora 42:
    https://bugzilla.redhat.com/show_bug.cgi?id=2418077
    while we wait for a response to that bug, let's go with treating
    this as a soft failure. We use an md5sum check to try and ensure
    that the output is *exactly* what we're expecting; if there are
    any unexpected messages, we'll still fail.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- tests/desktop_graphics_validation.pm
+++ tests/desktop_graphics_validation.pm
@@ -23,7 +23,30 @@ sub run {
     # https://bugzilla.redhat.com/show_bug.cgi?id=2416557
     type_string "exit\n", max_interval => 6;
     sleep 2;
-    assert_script_run 'vkcube --validate --c 10', max_interval => 6;
+    my $relnum = get_release_number;
+    if ($relnum != 42) {
+        assert_script_run 'vkcube --validate --c 10', max_interval => 6;
+        return;
+    }
+    # FIXME special handling for expected error on F42:
+    # https://bugzilla.redhat.com/show_bug.cgi?id=2418077
+    # remove when F42 is EOL, *or* that bug is fixed
+    my $res = script_run 'vkcube --validate --c 10 > /var/tmp/vkcubelog.txt', max_interval => 6;
+    # if it exited 0 we're fine (and we can drop this whole block now)
+    return unless ($res);
+    # *now* we can use a VT to check the failure mode
+    $self->root_console(tty => 3);
+    # for debugging (this check itself, and any unexpected failures)
+    upload_logs '/var/tmp/vkcubelog.txt';
+    # this is the md5sum of our expected failure mode on F42:
+    # WARNING : VALIDATION - Message Id Number: 582089644 | Message Id Name: WARNING-vkGetDeviceProcAddr-device
+    # 	vkGetDeviceProcAddr(): pName is trying to grab vkCreateDisplayPlaneSurfaceKHR which is an instance level function
+    assert_script_run 'echo "bc04b3d52198f0c42515808f27227975  /var/tmp/vkcubelog.txt" > expected.md5';
+    $res = script_run 'md5sum -c expected.md5';
+    if ($res) {
+        die 'Unexpected output from vkcube --validate! See vkcubelog.txt';
+    }
+    record_soft_failure 'Expected vkCreateDisplayPlaneSurfaceKHR error - see https://bugzilla.redhat.com/show_bug.cgi?id=2418077';
 }
 
 sub test_flags {
